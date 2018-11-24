55af0095bf77c5ea53425ab5b9b9ad92847c7d97
Touhid Mia <touhid@bitmascot.com>
2018-11-24 10:42:53 +0600

    Request Response

--- src/main/groovy/com/hmtmcse/gs/data/GsDomain.groovy
+++ src/main/groovy/com/hmtmcse/gs/data/GsDomain.groovy
@@ -2,6 +2,8 @@ package com.hmtmcse.gs.data
 
 class GsDomain {
 
+    public LinkedHashMap<String, GsDomainProperty> domainProperties = new LinkedHashMap<>();
+
     public List<GsDomainProperty> allProperties = new ArrayList<>()
     public List<GsDomainHasMany> hasMany = new ArrayList<>()
     public List<GsDomainBelongTo> belongsTo = new ArrayList<>()
