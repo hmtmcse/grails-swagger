1e7511008925061be8701d0748b7573a484fc1a6
Touhid Mia <touhid@bitmascot.com>
2018-12-02 11:04:57 +0600

    All Features

--- src/main/groovy/com/hmtmcse/swagger/definition/SwaggerConstant.java
+++ src/main/groovy/com/hmtmcse/swagger/definition/SwaggerConstant.java
@@ -19,11 +19,13 @@ public class SwaggerConstant {
 
     // Swagger Data Types
     public final static String SWAGGER_DT_INTEGER = "integer";
-    public final static String SWAGGER_DT_INTEGER_64 = "int64";
+    public final static String SWAGGER_DT_INTEGER_64 = "number";
     public final static String SWAGGER_DT_STRING = "string";
     public final static String SWAGGER_DT_STRING_DATE = "string";
     public final static String SWAGGER_DT_NUMBER = "number";
     public final static String SWAGGER_DT_BOOLEAN = "boolean";
     public final static String SWAGGER_DT_ARRAY = "array";
     public final static String SWAGGER_DT_OBJECT = "object";
+    public final static String SWAGGER_DT_DOUBLE = "double";
+    public final static String SWAGGER_DT_FLOAT = "float";
 }
